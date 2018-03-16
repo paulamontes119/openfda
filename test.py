e25ed3a2eae3cf9db244e090175c0c72a9696dd9
p.montes.2017 <paulamontes119@gmail.com>
2018-03-16 16:39:07 +0100

    Initial commit

---
+++ test.py
@@ -0,0 +1,19 @@
+a = 1
+b = 0
+
+
+print ("Hello from PyCharm!!! 1")
+print ("Hello from PyCharm!!! 2")
+print ("Hello from PyCharm!!! 3")
+
+print (a/b)
+
+
+print ("Hello from PyCharm!!! 4")
+print ("Hello from PyCharm!!! 5")
+print ("Hello from PyCharm!!! 6")
+print ("Hello from PyCharm!!! 7")
+print ("Hello from PyCharm!!! 8")
+
+print ("Hello from PyCharm!!! jkjkjkjk")
+
